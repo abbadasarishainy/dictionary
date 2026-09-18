9e9ad683bd1a86e136b1bc463c47eacce073d980
abbadasarishainy <abbadasarishainy@gmail.com>
2026-09-18 18:54:53 +0530

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,22 @@
+###dictionary
+d={}
+print(d)
+
+###possible ways to create dictionary
+d={"n":"s","a":20,"n":8}
+print(d)
+##
+d={1:10,2:20,3:30}
+print(d)
+##
+d={"1":10,"2":20,"3":30,"4":40}
+print(d)
+##
+d={[1,2]:10,[2,2]:20}
+print(d)
+###
+d={(10,1):10,(10,2):20}
+print(d)
+###
+d={(1,2):[10],(2,2):[20]}
+print(d)
